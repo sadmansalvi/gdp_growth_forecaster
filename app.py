e7dd610fb25fcba86f46b2b835946c0829594237
sadmansalvi <salimsadmansalvi@gmail.com>
2026-03-12 19:49:32 +0600

    .

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def optimize_budget(target_country_code, fdi_val, trade_val, total_budget_pct, m
     return opt_df.iloc[best_idx], preds[best_idx]
 
 # 4. App UI Header
-st.title("Forecast GDP Growth Rate of countries")
+st.title("Forecast GDP Growth Rate of Countries")
 st.markdown("### Machine Learning Engine for GDP Growth Rate Prediction & Budget Allocation(Based on Historical Data)")
 st.info(
     "**Disclaimer:** This tool utilizes an XGBoost machine learning model trained exclusively on historical macroeconomic data from 16 specific nations across three economic blocs: the **G7** (United States, Germany, Japan, United Kingdom, France, Italy, Canada), **BRICS** (Brazil, Russia, India, China, South Africa), and **South Asia** (Bangladesh, Pakistan, Sri Lanka, Nepal). "
